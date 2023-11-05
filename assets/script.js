0c46c2ef26e08c31f3581e033e3693805f936509
John Turner Dick <JD@Johns-MacBook-Pro.local>
2023-11-05 21:10:50 +0000

    final changes

--- assets/script.js
+++ assets/script.js
@@ -31,7 +31,36 @@ for (var x = 0; x < shapes.length; x++) {
   console.log(shapes[x]);
 }*/
 
-var topics = ["HTML", "CSS", "pentagon", "circle"];
-for (var x = 0; x < topics.length; x++) {
-  console.log(topics[x]);
+var topics = ["HTML", "CSS", "Git", "JavaScript"];
+
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
+
+console.log(randomTopic);
+
+function listTopics() {
+  for (var x = 0; x < topics.length; x++) {
+    console.log(topics[x]);
+  }
 }
+
+function selectTopic() {
+  if (randomTopic === "HTML") {
+    console.log("Let's study HTML!");
+  } else if (randomTopic === "CSS") {
+    console.log("Let's study CSS!");
+  } else if (randomTopic === "Git") {
+    console.log("Let's study Git!");
+  } else if (randomTopic === "JavaScript") {
+    console.log("Let's study JavaScript!");
+  } else {
+    console.log("Please try again!");
+  }
+}
+
+console.log("Here are the topics we learned in the pre-work:");
+
+listTopics();
+
+console.log("Which topic shall we learn today?");
+
+selectTopic();
